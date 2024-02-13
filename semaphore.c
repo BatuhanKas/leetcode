9a7af497c68b089c065a70806b3adbd7251ecdc3
BatuhanKas <batuhannkas@gmail.com>
2024-02-13 14:06:56 +0300

    update

--- semaphore.c
+++ semaphore.c
@@ -6,14 +6,16 @@
 /*   By: bkas <bkas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 12:06:19 by bkas              #+#    #+#             */
-/*   Updated: 2024/02/13 13:38:29 by bkas             ###   ########.fr       */
+/*   Updated: 2024/02/13 14:06:49 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <semaphore.h>
+#include <pthread.h>
 
 #define THREAD_NUM 4
+
 sem_t sem;
 
 void    *thread(void *arg) {
@@ -38,6 +40,29 @@ int main (int argc, char **argv) {
     // 1. sem_t *sem: semaforun adresi
     // 2. int pshared: 0 veya 1 olabilir. 0 ise semafor sadece process içinde paylaşılır. 1 ise semafor processler arasında paylaşılır.
     // 3. unsigned int value: semaforun başlangıç değeri
+    // sem init şu işe yarar.
+    // semaforu başlatır.
     sem_init(&sem, 0, 0);
+    // sem open parametreleri şunlardır:
+    // 1. const char *name: semaforun ismi
+    // 2. int oflag: semaforun açılma şekli. O_CREAT, O_EXCL, O_RDWR, O_RDONLY, O_WRONLY
+    // 3. mode_t mode: semaforun izinleri
+    // 4. unsigned int value: semaforun başlangıç değeri
+    // sem open şu işe yarar.
+    // semaforu açar.
     sem_open("sem", O_CREAT, 0644, 0);
+    for (int i = 0; i < THREAD_NUM; i++) {
+        pthread_join(th[i], NULL);
+    }
+    // sem destroy parametreleri şunlardır:
+    // 1. sem_t *sem: semaforun adresi
+    // sem destroy şu işe yarar.
+    // semaforu yok eder.
+    sem_destroy(&sem);
+    // sem close parametreleri şunlardır:
+    // 1. sem_t *sem: semaforun adresi
+    // sem close şu işe yarar.
+    // semaforu kapatır.
+    sem_close(&sem);
+    return (0);
 }
