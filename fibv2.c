d36089b6d2124029c42f99466cba125f6c19f0db
example <email@example.com>
2024-02-23 22:04:28 +0300

    update

--- fibv2.c
+++ fibv2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fibv2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bkas <bkas@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: batuhan <batuhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:18:45 by bkas              #+#    #+#             */
-/*   Updated: 2024/02/23 18:20:52 by bkas             ###   ########.fr       */
+/*   Updated: 2024/02/23 22:03:27 by batuhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 int main () {
     int firstval = 0;
-    int secval = 0;
+    int secval = 1;
+    int tmp = 0;
     int sum = 0;
-    int i = 1;
     while (1) {
-        secval = firstval;
-        secval = i;
-        
+        sum = secval + firstval;
+        if (sum > 4000000)
+            break ;
+        if (sum % 2 == 0)
+            tmp += sum;
+        printf("sum : %d\n", sum);
+        printf("tmp : %d\n", tmp);
+        firstval = secval;
+        secval = sum;
     }
 }
